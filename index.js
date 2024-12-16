1533d4791fd5b70e0ec10aa9b890854918ccf881
parthsoni <parths2004@duck.com>
2024-12-16 21:51:38 +0530

    code cleaning

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let xCount = document.querySelector('xWin');
 let oCount = document.querySelector('oWin');
 document.querySelector('#grid').addEventListener('click',(e)=>{
     var boxId = e.target.id;
-    console.log(boxId);
+    // console.log(boxId);
 	if(boxId != "")
 	{
 		if(state[boxId] == 1 || state[boxId] == 0)
@@ -40,8 +40,8 @@ function setWinner()
 		turn.style.visibility = "visible";
 		turn.innerText = "Congratulations to the winner! You can click 'Restart Game' for another match";
 		xCount++;
-		console.log(`X Winner = ${xCount}`);
-		console.log(`O Winner = ${oCount}`);
+		// console.log(`X Winner = ${xCount}`);
+		// console.log(`O Winner = ${oCount}`);
 		xWin.innerText = xCount;
 	}	
 	else
@@ -51,8 +51,8 @@ function setWinner()
 		turn.style.visibility = "visible";
 		turn.innerText = "Congratulations to the winner! You can click 'Restart Game' for another match";
 		oCount++;
-		console.log(`X Winner = ${xCount}`);
-		console.log(`O Winner = ${oCount}`);
+		// console.log(`X Winner = ${xCount}`);
+		// console.log(`O Winner = ${oCount}`);
 		oWin.innerText = oCount;
 	}	
 }
@@ -177,160 +177,3 @@ function restart()
 	state = [1,0,1,0,1,0,0,1,0];
 	flag = true;
 }
-
-/*
-    Considering Tied match situation
-    1 0 1
-    0 1 0
-    0 1 0
-
-let state = [1, 0, 1, 0, 0, 1, 1, 1, 0];
-let flag = true;
-const winnerDiv = document.getElementById('winner');
-
-/*
-
-1	0	1
-0	0	1
-1	1	0
-
-winner conditions
--- row
-
-state[0] == state[1]  && state[0] == state[2] -- row 1
-state[3] == state[4]  && state[3] == state[5] -- row 2
-state[6] == state[7]  && state[6] == state[8] -- row 3
-
--- col
-
-state[0] == state[3]  && state[0] == state[6] -- col 1
-state[1] == state[4]  && state[1] == state[7] -- col 2
-state[2] == state[5]  && state[2] == state[8] -- col 3
-
--- dia
-
-state[0] == state[4]  && state[0] == state[8] -- dia 1
-state[2] == state[4]  && state[2] == state[6] -- dia 3
-
-
-document.querySelector('.container').addEventListener('click', (e) => {
-	var boxId = e.target.id;
-	if (boxId != "") {
-		if (state[boxId] == 1 ||  state[boxId] == 0)
-		{
-			if (flag)
-			{
-				e.target.innerHTML = "X";
-				state[boxId] = "X";
-			}
-			else
-			{
-				e.target.innerHTML = "O";
-				state[boxId] = "O";
-			}
-			flag = !flag;
-			// console.log(state);
-			checkWinner(!flag);
-		}
-		// else
-		// {
-		// 	alert("Not Allowed !! ");
-		// }
-	}
-});
-
-
-function checkWinner(flag)
-{
-	if(state[0] == state[1]  && state[0] == state[2])
-	{
-		// alert("Row 1 Winner");
-		if(flag)
-			winnerDiv.innerText = "X Winner";
-		else
-			winnerDiv.innerText = "O Winner";
-		setPattern(0, 1, 2);
-		setDisabled();
-	}
-	else if(state[3] == state[4]  && state[3] == state[5])
-	{
-		alert("Row 2 Winner");
-	}
-	else if(state[6] == state[7]  && state[6] == state[8])
-	{
-		alert("Row 3 Winner");
-	}
-	else if(state[0] == state[3]  && state[0] == state[6])
-	{
-		alert("Col 1 Winner");
-	}
-	else if(state[1] == state[4]  && state[1] == state[7])
-	{
-		alert("Col 2 Winner");
-	}
-	else if(state[2] == state[5]  && state[2] == state[8])
-	{
-		alert("Col 3 Winner");
-	}
-	else if(state[0] == state[4]  && state[0] == state[8])
-	{
-		alert("dia 1 Winner");
-	}
-	else if(state[2] == state[4]  && state[2] == state[6])
-	{
-		alert("dia 2 Winner");
-	}
-	else
-	{
-		// alert("Its Draw !!");
-		let count = 9;
-		for(let i = 0; i<9; i++)
-		{
-			if(state[i]=="X" || state[i]=="O")
-			{
-				count--;
-			}
-		}
-
-		if(count == 0)
-		{
-			winnerDiv.innerText = "Its Draw !!";
-		}
-	}
-}
-
-function setDisabled()
-{
-	for(let i=0; i<9; i++)
-	{
-		if(state[i] == 1 || state[i] == 0)
-		{
-			state[i] = null;
-		}
-	}
-}
-
-function resetUI()
-{
-	for(let i=0; i<9; i++)
-	{
-		document.getElementById(i).innerText = "";
-		document.getElementById(i).style.background = "#ff8080";
-	}
-
-	state = [1, 0, 1, 0, 0, 1, 1, 1, 0];
-	flag = true;
-}
-
-function restartGame()
-{
-	window.location.reload();
-}
-
-function setPattern(a, b, c)
-{
-	document.getElementById(a).style.backgroundColor = "green";
-	document.getElementById(b).style.backgroundColor = "green";
-	document.getElementById(c).style.backgroundColor = "green";
-}
-*/
